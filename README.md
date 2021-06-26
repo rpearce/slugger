30f5504959cc266fc0e05fcbd2bfa0bed8ef085f
Robert W. Pearce <me@robertwpearce.com>
2021-06-26 10:42:19 -0400

    couple of dev  readme updates; add some slugger tests

--- README.md
+++ README.md
@@ -10,10 +10,30 @@ WIP
 
 ## Usage
 
-## Library
+### Library
 
 WIP
 
-## Executable
+### Executable
 
 WIP
+
+## Development
+
+Get into a nix dev environment:
+
+```sh
+λ nix develop
+```
+
+Run the tests from GHCi:
+
+```sh
+[nix]λ cabal repl slugger-test
+[ghci]λ :main
+# test output prints here
+
+# make some changes, then...
+[ghci]λ :reload
+[ghci]λ :main
+```
--- test/SluggerTest.hs
+++ test/SluggerTest.hs
@@ -8,42 +8,44 @@ import qualified Slugger
 
 main :: IO ()
 main = hspec $ do
+    -- Slugger.toSlugString is a tiny wrapper around Slugger.toSlugText
     describe "Slugger.toSlugString" $ do
 
-        context "when given an empty string" $ do
-            it "returns an empty string" $ do
-                Slugger.toSlugString "" `shouldBe` ""
-
-        context "when given a string with empty spaces" $ do
-            it "returns an empty string" $ do
-                Slugger.toSlugString "  " `shouldBe` ""
-
-        context "when given uppercase words" $ do
-            it "returns words lowercased and hyphenated" $ do
-                Slugger.toSlugString "HELLO GREAT WORLD" `shouldBe`
-                    "hello-great-world"
-
-        context "when given uppercase words with symbols" $ do
-            it "returns words lowercased and hyphenated" $ do
-                Slugger.toSlugString "HELLO, WORLD!!!" `shouldBe` "hello-world"
+        it "works" $ do
+            Slugger.toSlugString "HELLO, WORLD!!!" `shouldBe` "hello-world"
 
 
     describe "Slugger.toSlugText" $ do
 
-        context "when given an empty string" $ do
+        context "when an empty string" $ do
             it "returns an empty string" $ do
                 Slugger.toSlugText (T.pack "") `shouldBe` T.pack ""
 
-        context "when given a string with empty spaces" $ do
+        context "when a string with empty spaces" $ do
             it "returns an empty string" $ do
                 Slugger.toSlugText (T.pack "  ") `shouldBe` T.pack ""
 
-        context "when given uppercase words" $ do
+        context "when uppercase words" $ do
             it "returns words lowercased and hyphenated" $ do
                 Slugger.toSlugText (T.pack "HELLO GREAT WORLD") `shouldBe`
                     T.pack "hello-great-world"
 
-        context "when given uppercase words with symbols" $ do
+        context "when uppercase words with symbols" $ do
             it "returns words lowercased and hyphenated" $ do
                 Slugger.toSlugText (T.pack "HELLO, WORLD!!!") `shouldBe`
                     T.pack "hello-world"
+
+        context "when leading & trailing whitespace" $ do
+            it "does nothing with the extra whitespace" $ do
+                Slugger.toSlugText (T.pack "  Hey world!   ") `shouldBe`
+                    T.pack "hey-world"
+
+        context "when repeated inner whitespace" $ do
+            it "treats the repeated inner whitespace as a single space" $ do
+                Slugger.toSlugText (T.pack "Hey there,  world!") `shouldBe`
+                    T.pack "hey-there-world"
+
+        --context "when invalid URI letters provided" $ do
+        --    it "returns words lowercased and hyphenated" $ do
+        --        Slugger.toSlugText (T.pack "¿Qué pasó?") `shouldBe`
+        --            T.pack "que-paso"
